b4acc6f466a348826901c2e29e9a55fe3613b0e7
dies178 <73028434+dies178@users.noreply.github.com>
2021-06-30 18:29:04 +0200

    Delete Bot-codigo-libre directory

--- Bot-codigo-libre/src/commands/prueba.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'prueba',
-	description: 'esto ni hace falta que se use, pero es para ayudarte',
-	execute(message, args) {
-		message.channel.send("Buenas :D");
-	},
-};
--- Bot-codigo-libre/src/eventos/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute() {
-		console.log(`${message.client.user.usernsame} esta encendido`);
-	},
-};
